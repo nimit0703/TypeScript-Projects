52191b629f5a1287a95a90661ed22e46f21ef6c9
nimit0703 <nimit.190410107143@gmail.com>
2023-09-27 16:55:37 +0530

    pokemon app phase 1

---
+++ marvel-api/public/app.js
@@ -0,0 +1,47 @@
+import { Pokemon } from "./classes/Pokemon.js";
+function getPokemonById(id) {
+    return fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+        .then((responce) => {
+        return responce.json();
+    })
+        .then((data) => {
+        let name = data.name;
+        let img = data.sprites.other.dream_world.front_default;
+        let types = data.types;
+        let abilities = data.abilities;
+        let forms = data.forms;
+        let poke = new Pokemon(name, img, types, abilities, forms);
+        console.log(poke);
+        return poke;
+    });
+}
+window.onload = function () {
+    const modal = document.getElementById("modal");
+    const card = document.getElementById("card");
+    const closeBtn = document.getElementById("close");
+    const pokemonSec = document.getElementById("pokemons");
+    for (let i = 0; i < 20; i++) {
+        const id = randomNumber(1, 500);
+        getPokemonById(id).then((pokemon) => {
+            const img = document.createElement("img");
+            img.src = pokemon.img;
+            img.setAttribute("data-pokemon-id", id.toString());
+            img.addEventListener("click", function () {
+                const pokemonId = this.getAttribute("data-pokemon-id");
+                const cardImg = document.getElementById('cardImg');
+                cardImg.src = pokemon.img;
+                const pokename = document.getElementById('pokeName');
+                pokename.innerText = pokemon.name;
+                modal.style.display = "block";
+            });
+            pokemonSec.appendChild(img);
+        });
+    }
+    closeBtn.addEventListener("click", function () {
+        modal.style.display = "none";
+    });
+};
+function randomNumber(min, max) {
+    // min and max included
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
---
+++ marvel-api/src/classes/Pokemon.ts
@@ -0,0 +1,12 @@
+export class Pokemon {
+  then: any;
+  constructor(
+    public name: string,
+    public img: string,
+    public type: string[],
+    public abilities: string[],
+    public forms: string[]
+  ) {
+
+  }
+}
