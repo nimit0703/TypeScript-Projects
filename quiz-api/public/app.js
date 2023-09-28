3fd846c65bfb34f04f2dfc80d78c7e60a08cfb46
nimit0703 <nimit.190410107143@gmail.com>
2023-09-28 12:00:22 +0530

    template design phase 1 done

--- quiz-api/public/app.js
+++ quiz-api/public/app.js
@@ -1,1 +1,7 @@
 "use strict";
+fetch("https://opentdb.com/api.php?amount=1&category=18&difficulty=medium&type=multiple")
+    .then((responce) => {
+    return responce.json();
+}).then((data) => {
+    console.log(data);
+});
---
+++ quiz-api/src/app.ts
@@ -0,0 +1,7 @@
+
+fetch("https://opentdb.com/api.php?amount=1&category=18&difficulty=medium&type=multiple")
+.then((responce)=>{
+    return responce.json();
+}).then((data)=>{
+    console.log(data)
+})
